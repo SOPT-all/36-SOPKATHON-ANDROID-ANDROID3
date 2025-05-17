d1fd03b7e7f93f3b3725e426deda246e74fab5b5
Dongmin <pdm001125@naver.com>
2025-05-17 22:51:28 +0900

    [del] 주석 제거

--- app/src/main/java/org/sopt/a36_sopkathon_android_android3/presentation/dongmin/DongminScreen.kt
+++ app/src/main/java/org/sopt/a36_sopkathon_android_android3/presentation/dongmin/DongminScreen.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.sopt.a36_sopkathon_android_android3.ui.theme._36SOPKATHONANDROIDANDROID3Theme
 
