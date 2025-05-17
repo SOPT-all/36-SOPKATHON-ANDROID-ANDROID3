d237d71f5932328f6a7a937f5d35e8ec90014c58
Dongmin <pdm001125@naver.com>
2025-05-18 00:15:10 +0900

    [feat] 서버통신 연결

--- app/src/main/java/org/sopt/a36_sopkathon_android_android3/presentation/dongmin/DongminScreen.kt
+++ app/src/main/java/org/sopt/a36_sopkathon_android_android3/presentation/dongmin/DongminScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,6 +18,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
@@ -38,46 +40,49 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
-import kotlinx.coroutines.launch
+import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import org.sopt.a36_sopkathon_android_android3.ui.theme._36SOPKATHONANDROIDANDROID3Theme
 
 @Composable
 fun DongminRoute(
     navigateToMinseo: () -> Unit,
+    viewModel: DongminViewModel = viewModel(),
 ) {
     DongminScreen(
-        navigateToMinseo = navigateToMinseo
+        navigateToMinseo = navigateToMinseo,
+        dialogState = viewModel.dialogState,
+        getDialogState = viewModel::getDialogState
     )
 }
 
 @Composable
 private fun DongminScreen(
     navigateToMinseo: () -> Unit,
-    modifier: Modifier = Modifier,
+    dialogState: DialogState,
+    getDialogState: () -> Unit = {},
 ) {
     val scope = rememberCoroutineScope()
     var isOpenDialog by remember { mutableStateOf(false) }
 
     if (isOpenDialog) {
         StoryDialog(
-            title = "경상남도 창원시 마산 회원구 양덕2동 김치찜 장인 박동민씨의 레시피",
-            story = "김치찜은 김치와 돼지고기를 함께 찌는 요리로, 김치의 깊은 맛과 돼지고기의 풍미가 어우러져 맛있습니다. 특히, 마산의 김치찜은 그 지역 특유의 매운 양념과 신선한 재료로 유명합니다. 박동민씨는 이 전통적인 레시피를 현대적으로 재해석하여, 더욱 부드럽고 깊은 맛을 자랑하는 김치찜을 만들어냅니다." +
-                    "이 김치찜은 깊은맛과 풍미가 어우러져, 밥과 함께 먹으면 더욱 맛있습니다. 또한, 김치찜은 건강에도 좋고, 특히 겨울철에 따뜻하게 즐기기 좋은 요리입니다. ",
+            imageUrl = dialogState.imageUrl,
+            name = dialogState.name,
+            title = dialogState.title,
+            story = dialogState.story,
             onDismissRequest = {
                 isOpenDialog = false
             },
             onRetry = {
-                scope.launch {
-                    isOpenDialog = false
-                }
-
+                isOpenDialog = false
             },
             onRecipe = navigateToMinseo
         )
     }
 
     Column(
-        modifier = modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -106,20 +111,21 @@ private fun DongminScreen(
                 .background(Color.Black)
                 .clickable(
                     onClick = {
+                        getDialogState()
                         isOpenDialog = true
                     }
                 )
         )
 
 
-            Text(
-                text = if (!isOpenDialog) "탭해서 특별한 레시피 보러가기" else "",
-                modifier = Modifier
-                    .clickable(
-                        onClick = navigateToMinseo
-                    )
-                    .padding(bottom = 20.dp)
-            )
+        Text(
+            text = if (!isOpenDialog) "탭해서 특별한 레시피 보러가기" else "",
+            modifier = Modifier
+                .clickable(
+                    onClick = navigateToMinseo
+                )
+                .padding(bottom = 20.dp)
+        )
 
     }
 
@@ -128,6 +134,8 @@ private fun DongminScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun StoryDialog(
+    imageUrl: String,
+    name: String,
     title: String,
     story: String,
     onDismissRequest: () -> Unit,
@@ -149,6 +157,25 @@ private fun StoryDialog(
                     .fillMaxWidth()
                     .padding(16.dp),
             ) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 8.dp),
+                    )
+
+                    Text(
+                        text = name,
+                    )
+                }
+
                 Text(
                     text = title,
                     modifier = Modifier.fillMaxWidth()
@@ -203,7 +230,8 @@ private fun StoryDialog(
 private fun PreviewMinseoScreen() {
     _36SOPKATHONANDROIDANDROID3Theme {
         DongminScreen(
-            navigateToMinseo = {}
+            navigateToMinseo = {},
+            dialogState = DialogState()
         )
     }
 }
